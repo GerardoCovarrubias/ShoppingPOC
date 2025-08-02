3a8cf3989b937b53b5ca362d1122c48b9647c9b6
Gerardo Covarrubias <briancovarrubiasmorales@outlook.com>
2025-08-01 18:47:32 -0600

    Add project files.

---
+++ Models/Sales.cs
@@ -0,0 +1,12 @@
+﻿namespace Models
+{
+    public class Sales
+    { 
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int QuantitySold { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime SaleDate { get; set; }
+      
+    }
+}
---
+++ Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿namespace Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string Location { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public Stock(string productName, int quantity, string location, DateTime lastUpdated)
+        {
+            ProductName = productName;
+            Quantity = quantity;
+            Location = location;
+            LastUpdated = lastUpdated;
+        }
+    }
+}
