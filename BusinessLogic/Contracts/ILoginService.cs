8b65f3d0e9e69b8a8427d710e6d0aea1d12bca9e
Isaí Yepez Gómez <isaiyepez@gmail.com>
2025-09-14 21:15:02 -0600

    Progress with Login backend

---
+++ BusinessLogic/Contracts/ILoginService.cs
@@ -0,0 +1,9 @@
+﻿namespace BusinessLogic.Contracts
+{
+    public interface ILoginService
+    {
+        Task<bool> ValidateUserAsync(string username, string password);
+        
+        Task ChangePasswordAsync(string username, string newPassword);
+    }
+}
---
+++ BusinessLogic/Services/LoginService.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLogic.Contracts;
+using ShoppingPOC.Data;
+
+namespace BusinessLogic.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public LoginService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task ChangePasswordAsync(string username, string newPassword)
+        {
+            var user = _dbContext. User.FirstOrDefault(u => u.Username == username);
+        }
+
+        public Task<bool> ValidateUserAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
