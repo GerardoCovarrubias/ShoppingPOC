39b7cc846125c67cc0b6f46d3969df5c74d492f0
Brian Gerardo Covarrubias <briancovarrubiasmorales@outlook.com>
2025-09-27 14:01:21 -0600

    Progress Login

---
+++ ShoppingPOC/wwwroot/Java/show.js
@@ -0,0 +1,23 @@
+﻿//namespace ShoppingPOC.wwwroot.Java {
+//    public class show {
+//    }
+//}
+
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.getElementById('togglePasswordIcon');
+
+    // Alterna el tipo de input
+    const isPassword = passwordInput.type === 'password';
+    passwordInput.type = isPassword ? 'text' : 'password';
+
+    // Alterna las clases del ícono
+    if (isPassword) {
+        toggleIcon.classList.remove('bi-eye-slash');
+        toggleIcon.classList.add('bi-eye');
+    } else {
+        toggleIcon.classList.remove('bi-eye');
+        toggleIcon.classList.add('bi-eye-slash');
+    }
+}
