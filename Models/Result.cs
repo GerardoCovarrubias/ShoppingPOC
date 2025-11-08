4d8ce2280a3b6b9e50d31fbcb5172777f30c1adc
Brian Gerardo Covarrubias <briancovarrubiasmorales@outlook.com>
2025-11-08 16:32:53 -0600

    Task OPS-19 completed (#7)
    * Task OPS-19 completed
    
    * Comments fixed

---
+++ Models/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace Models
+{
+   
+    public class Result
+    {
+        public ResultStatusCode StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public Result(ResultStatusCode statusCode, string errorMessage = "")
+        {
+            StatusCode = statusCode;
+            ErrorMessage = errorMessage;
+        }
+
+    }
+}
