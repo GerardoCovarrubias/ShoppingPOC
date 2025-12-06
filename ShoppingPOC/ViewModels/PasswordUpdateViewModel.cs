fb5fb0e575f63d8e23b9c23e34357a204c44b23c
Brian Gerardo Covarrubias <briancovarrubiasmorales@outlook.com>
2025-12-06 12:36:51 -0600

    Finished review the complete login flow

---
+++ ShoppingPOC/ViewModels/PasswordUpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingPOC.ViewModels
+{
+    public class PasswordUpdateViewModel
+    {
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string NewPassword { get; set; }
+        
+        [Required(ErrorMessage = "Confirmation is required.")]
+        [Compare(nameof(NewPassword), ErrorMessage = "Passwords do not match")]
+        public string PasswordConfirmation { get; set; }
+
+    }
+}
